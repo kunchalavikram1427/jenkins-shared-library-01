9a2bc4fb280311ebaa62eaf46f6d0b543f6ed2d7
Vikram <kunchalavikram1427@gmail.com>
2024-10-27 18:10:38 +0530

    test

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -41,9 +41,42 @@ def pushChanges(String credentialsId = '', String commitMessage = 'Automated com
     return featureBranch // Return the branch name for later use
 }
 
+// def createPullRequest(String defaultBranch, String featureBranch, String credentialsId) {
+//     def prTitle = "Merge ${featureBranch} into ${defaultBranch}"
+//     def prBody = "This PR merges the feature branch into ${defaultBranch}."
+
+//     // Get GitHub credentials
+//     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+//         // Construct the JSON payload for the PR
+//         def jsonPayload = """{
+//             "title": "${prTitle}",
+//             "body": "${prBody}",
+//             "head": "${featureBranch}",
+//             "base": "${defaultBranch}"
+//         }"""
+
+//         // Make the API call to create the PR
+//         sh """
+//             curl -L -X POST \
+//                 -H "Authorization: Bearer ${GIT_PASS}" \
+//                 -H "Accept: application/vnd.github+json" \
+//                 -H "X-GitHub-Api-Version: 2022-11-28" \
+//                 -d '${jsonPayload}' \
+//                 https://api.github.com/repos/${env.GIT_REPO.split('/')[3]}/${env.GIT_REPO.split('/')[4]}/pulls
+//         """
+//     }
+    
+//     echo "Pull request created from '${featureBranch}' to '${defaultBranch}'."
+// }
+
 def createPullRequest(String defaultBranch, String featureBranch, String credentialsId) {
     def prTitle = "Merge ${featureBranch} into ${defaultBranch}"
     def prBody = "This PR merges the feature branch into ${defaultBranch}."
+    
+    // Extract the owner and repo from GIT_REPO
+    def repoParts = env.GIT_REPO.replaceAll(/\.git$/, '').split('/')
+    def owner = repoParts[-2]
+    def repo = repoParts[-1]
 
     // Get GitHub credentials
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
@@ -55,14 +88,14 @@ def createPullRequest(String defaultBranch, String featureBranch, String credent
             "base": "${defaultBranch}"
         }"""
 
-        // Make the API call to create the PR
+        // Make the API call to create the PR with basic authentication
         sh """
             curl -L -X POST \
-                -H "Authorization: Bearer ${GIT_PASS}" \
+                -u ${GIT_USER}:${GIT_PASS} \
                 -H "Accept: application/vnd.github+json" \
                 -H "X-GitHub-Api-Version: 2022-11-28" \
                 -d '${jsonPayload}' \
-                https://api.github.com/repos/${env.GIT_REPO.split('/')[3]}/${env.GIT_REPO.split('/')[4]}/pulls
+                https://api.github.com/repos/${owner}/${repo}/pulls
         """
     }
     
