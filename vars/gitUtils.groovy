dcbb86ef2fbaf2ddfebc21dbe91e11c8f508a118
Vikram <kunchalavikram1427@gmail.com>
2024-10-27 13:40:23 +0530

    test

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -20,19 +20,22 @@ def pushChanges(String credentialsId = '', String commitMessage = 'Automated com
     sh "git config --global user.email jenkins@test.com"
     sh "git config --global user.name jenkins"
     sh "git commit -m '${commitMessage}'"
-    
+
+    // Construct the branch name using the Jenkins build number
+    def featureBranch = "feature-${env.BUILD_NUMBER}"
+
     if (credentialsId) {
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
             // Build the command as a Groovy string
             def remoteUrl = "\$(git config --get remote.origin.url | sed 's/https:\\/\\///g')"
-            def branchName = "\$(git rev-parse --abbrev-ref HEAD)"
-            def pushCommand = "git push https://${GIT_USER}:${GIT_PASS}@${remoteUrl} HEAD:${branchName}"
+            def pushCommand = "git push https://${GIT_USER}:${GIT_PASS}@${remoteUrl} HEAD:refs/heads/${featureBranch}"
             
             // Execute the command
             sh pushCommand
         }
     } else {
-        sh "git push origin \$(git rev-parse --abbrev-ref HEAD)"
+        sh "git push origin HEAD:refs/heads/${featureBranch}"
     }
-    echo "Changes pushed to the repository."
+    echo "Changes pushed to branch '${featureBranch}'."
 }
+
